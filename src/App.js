6dec2ab9f25ee3c550b7ff502b49fb308577ca4e
Brian MULIER <brian.mulier.partner@decathlon.com>
2021-12-02 00:38:17 +0100

    ft Working socket

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import React, {useContext, useEffect, useState} from "react";
+import {SocketContext} from './context/client-socket';
+
+
+function App(props) {
+    const [client_id, setClientId] = useState(NaN);
+    const socket = useContext(SocketContext);
+
+    useEffect(() => {
+        socket.on("client_number", (data) => {
+            console.log(data);
+            setClientId(data.id);
+        })
+    })
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    You're the client number {client_id}
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
--- websocket_server.js
+++ websocket_server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const { Server:socketServer } = require("socket.io");
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const PORT = process.env.PORT || 3001;
 
 const server = express()
-    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = new socketServer(server);
 
+let connectedClients = 0;
+
 io.on('connection', (socket) => {
     console.log('Client connected');
+    socket.emit("client_number", {
+        "id": ++connectedClients
+    })
     socket.on('disconnect', () => console.log('Client disconnected'));
 });
