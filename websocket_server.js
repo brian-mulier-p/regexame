d538f4d87ed32120fbdd5f47cd5c7dfc86a47a78
Brian MULIER <brian.mulier.partner@decathlon.com>
2021-12-02 01:45:57 +0100

    Build only once on heroku deploy

--- websocket_server.js
+++ websocket_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { Server:socketServer } = require("socket.io");
 const path = require("path")
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = app
