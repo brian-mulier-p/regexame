26c9693209d82d66f50521e2486566ffedcd36cb
Brian MULIER <brian.mulier.partner@decathlon.com>
2021-12-02 01:34:01 +0100

    Working CLIENT / SERVER on same port

--- src/context/client-socket.js
+++ src/context/client-socket.js
@@ -1,5 +1,5 @@
 import socketio from "socket.io-client";
 import React from "react";
 
-export const socket = socketio.connect("http://localhost:3001", { transports : ['websocket'] });
+export const socket = socketio.connect({ transports : ['websocket'] });
 export const SocketContext = React.createContext();
--- websocket_server.js
+++ websocket_server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const { Server:socketServer } = require("socket.io");
+const path = require("path")
 
 const PORT = process.env.PORT || 3001;
 
-const server = express()
+const app = express();
+const server = app
+    .use(express.static(path.join(__dirname, 'build')))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const io = new socketServer(server);
 
